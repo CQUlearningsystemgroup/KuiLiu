90a82088f00910bcaea6c3617bd95e5702b2d054
CQUlearningsystemgroup <45023422+CQUlearningsystemgroup@users.noreply.github.com>
2020-11-12 16:47:35 +0800

    Add files via upload

---
+++ src/DSQ.m
@@ -0,0 +1,28 @@
+function [xq] = DSQ(x, bits)
+alpha = 0.5003;
+cur_max = max(x);
+cur_min = min(x);
+
+delta = (cur_max - cur_min)/2.0^bits;
+interval = floor((x - cur_min)/delta);
+
+mi = (interval + 0.5)*delta + cur_min;
+
+% phi_function
+s = 1/(1-alpha);
+k = log(2/alpha - 1)*(1/delta);
+x = tanh((x-mi)*k)*s;
+
+% sign_function
+max_s = max(x);
+min_s = min(x);
+delta_s = max_s - min_s;
+x = x / delta_s + 0.5;
+x_round = round(x);
+x = x_round*2 - 1;
+
+% dequantization_function
+xq = ((x+1)/2 + interval)*delta + cur_min;
+
+
+
---
+++ src/ccn.m
@@ -0,0 +1,23 @@
+function [CCN] = ccn(CS0, i)
+    yolo_weights = load('yolo_2200.txt');
+
+    for K_N = i:i
+        rgb = 0;
+        kernel = zeros(3,3);
+        for kernel_line = 1:3  
+            for kernel_row = 1:3  
+                turn_weights = (K_N-1)*27+rgb*9+kernel_line*3-3+kernel_row;
+                kernel(kernel_line ,kernel_row) = yolo_weights(turn_weights);
+            end
+        end  
+    end
+    k_rate = (14)^2/CS0;
+    fin(1,1) = 1;
+    % fin返回抽样数据的坐标
+    for res = 1:(CS0-1)            
+        fin(res+1,1) = 1+round(k_rate*res); 
+    end
+    % mat_trans()是将核矢量化
+    CCN = mat_trans(kernel,16); 
+    CCN = CCN(fin,:);
+end
